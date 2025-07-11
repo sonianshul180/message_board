3a00001e8420704bbca898f9f5ed3f15b72b408b
Hitesh Goyal <hgoyal@q3tech.com>
2025-07-11 16:34:44 +0530

    Pushed docker file

--- two_tier_project/two_tier_project/settings.py
+++ two_tier_project/two_tier_project/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'two_tier_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'message_board_db',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
